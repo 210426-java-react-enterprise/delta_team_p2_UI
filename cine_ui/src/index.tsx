fdbee0357f959a175c41cd0d5e9b3969560c02a4
DanceableBleats <KylePlummerBSCS@gmail.com>
2021-06-08 20:21:50 -0400

    End to end prototyping, registration to database

--- cine_ui/src/index.tsx
+++ cine_ui/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AuthScreen from './AuthScreen';
+import { AuthComponent } from './components/AuthComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthScreen />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
