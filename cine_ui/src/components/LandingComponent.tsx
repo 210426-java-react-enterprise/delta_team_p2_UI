a339435b363f7319f2a597ae4d93c6e7b61acc2b
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-06-09 21:34:09 -0700

    Added some paddding and formating

--- cine_ui/src/components/LandingComponent.tsx
+++ cine_ui/src/components/LandingComponent.tsx
@@ -2,7 +2,7 @@
 import React from "react"
 import {ListGroup, Card, Container, Row, Col, Carousel} from "react-bootstrap"
 
-
+//TODO: Setup interface to take a created User property
 interface IUserrops{
 
 }
@@ -11,55 +11,58 @@ export function LandingComponent(props: any){
 
     const listItems = ["A", "B", "C"]
 
+    const fakeMovieList = ["movie1", "movie2", "movie3"]
+
+    //TODO: Change these values to match the user's values
     return(
         <>
 
             <Container fluid>
                 <Row>
                     <Col xs = "10">
-                                <Card>
-                                    <Carousel>
-                                    <Carousel.Item>
-                                        test Item 1-1
-                                    </Carousel.Item>
-                                    <Carousel.Item>
-                                        test Item 1-2
-                                    </Carousel.Item>
-                                    <Carousel.Item>
-                                        test Item 1-3
-                                    </Carousel.Item>
-                                    </Carousel>
-                                </Card>
-                                <br/>
-                                <Card>
-                                    <Carousel>
-                                    <Carousel.Item>
-                                        test Item 2-1
-                                    </Carousel.Item>
-                                    <Carousel.Item>
-                                        test Item 2-2
-                                    </Carousel.Item>
-                                    <Carousel.Item>
-                                        test Item 2-3
-                                    </Carousel.Item>
-                                    </Carousel>
-                                </Card>
+                        <br/>
+                        <Card>
+                            <Card.Header>Movies To Watch List</Card.Header>
+                                <Carousel>
+                                    {fakeMovieList.map(fakeMovieList => (
+                                        <Carousel.Item>
+                                            {fakeMovieList}
+                                        </Carousel.Item>
+                                    ))}
+                                </Carousel>
+                        </Card>
 
+                        <br/>
+                        <Card>
+                        <Card.Header>Movie Watch History</Card.Header>
+                            <Carousel>
+                            <Carousel.Item>
+                                test Item 2-1
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                test Item 2-2
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                test Item 2-3
+                            </Carousel.Item>
+                            </Carousel>
+                        </Card>
 
                     </Col>
-                    <Col>
-                    <Card style={{height: '50em'}}>
-                        <Card>
-                        <Card.Header>Friends</Card.Header>
-                            <ListGroup className="seacrh=results" variant="flush">
-                                {listItems.map(listItems => (
-                                    <ListGroup.Item>
-                                        {listItems}
-                                    </ListGroup.Item>
-                                ))}
-                                <ListGroup.Item>----------</ListGroup.Item>
-                            </ListGroup>
-                        </Card> 
+                    <Col className="pt-4">
+                    <Card style={{height: "57rem"}}>
+                    <Card.Header>Friends</Card.Header>
+                        <div className="justify-content-center">
+                                <ListGroup className="seacrh=results" variant="flush">
+                                    {listItems.map(listItems => (
+                                        <ListGroup.Item>
+                                            {listItems}
+                                        </ListGroup.Item>
+                                    ))}
+                                    <ListGroup.Item>----------</ListGroup.Item>
+                                </ListGroup>
+                        </div>
+                        
                     </Card>
                         
                     </Col>
