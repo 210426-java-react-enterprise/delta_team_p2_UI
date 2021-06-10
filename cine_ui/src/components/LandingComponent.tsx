8ca4254a0be7530448ab17aca3e0eea07d970050
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-06-10 14:54:38 -0700

    Edited carousel to be able to show multiple elements

--- cine_ui/src/components/LandingComponent.tsx
+++ cine_ui/src/components/LandingComponent.tsx
@@ -1,6 +1,7 @@
 
 import React from "react"
-import {ListGroup, Card, Container, Row, Col, Carousel} from "react-bootstrap"
+
+import {ListGroup, Card, Container, Row, Col, Carousel, CardGroup} from "react-bootstrap"
 
 //TODO: Setup interface to take a created User property
 interface IUserrops{
@@ -11,40 +12,50 @@ export function LandingComponent(props: any){
 
     const listItems = ["A", "B", "C"]
 
-    const fakeMovieList = ["movie1", "movie2", "movie3"]
+    const fakeMovieList = ["movie1", "movie2", "movie3", "movie 4", "movie 5", "movie6"]
+
+    const rows = fakeMovieList.reduce(function (rows: any, key, index) { 
+        return (index % 2 == 0 ? rows.push([key]) 
+          : rows[rows.length-1].push(key)) && rows;
+      }, []);
+
+      console.log(rows)
+
 
     //TODO: Change these values to match the user's values
     return(
-        <>
 
+        <>
             <Container fluid>
                 <Row>
                     <Col xs = "10">
                         <br/>
                         <Card>
                             <Card.Header>Movies To Watch List</Card.Header>
-                                <Carousel>
+                                <Carousel >
                                     {fakeMovieList.map(fakeMovieList => (
                                         <Carousel.Item>
                                             {fakeMovieList}
                                         </Carousel.Item>
                                     ))}
                                 </Carousel>
-                        </Card>
 
+                        </Card>
                         <br/>
                         <Card>
                         <Card.Header>Movie Watch History</Card.Header>
                             <Carousel>
-                            <Carousel.Item>
-                                test Item 2-1
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                test Item 2-2
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                test Item 2-3
-                            </Carousel.Item>
+                                {rows.map((twoMovies: any) => (
+                                    <Carousel.Item>
+                                        <CardGroup>
+                                            {twoMovies.map((movie:any) => (
+                                                <Card>
+                                                    {movie}
+                                                </Card>
+                                            ))}
+                                        </CardGroup>
+                                    </Carousel.Item>
+                                ))}
                             </Carousel>
                         </Card>
 
