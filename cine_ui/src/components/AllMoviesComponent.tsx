6cedfdbd9d407efc85338c9f34b0734963a0a00c
Juan Mendoza <juanmendoza0309@gmail.com>
2021-06-10 19:02:46 -0700

    onClick calles webapp to get more details on a movie(allmoviecomponenet)

--- cine_ui/src/components/AllMoviesComponent.tsx
+++ cine_ui/src/components/AllMoviesComponent.tsx
@@ -1,4 +1,5 @@
 import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import { moreDetails } from "../remote/moreInfo-service";
 
 interface IAllMoviesProps{
     //this prop refers to what we will use to call in App state level
@@ -6,8 +7,11 @@ interface IAllMoviesProps{
 }
 
 export function AllMoviesComponent(props:IAllMoviesProps){
-    function alterMessage(){
-        alert("selected movieSelected");
+
+    let moreInformation = async(id:number) => {
+        console.log("searched Movied", id);
+        await moreDetails(id);
+        console.log('complete');
     }
     //click or double for more details on movies serperate call to another api(/movieDetails/id)--->just id(IMdb [tt}])
     return(
@@ -20,11 +24,11 @@ export function AllMoviesComponent(props:IAllMoviesProps){
                     i%2===0
                     ?
                     <ListGroup>
-                        <ListGroup.Item action variant="dark">{i} {movie.title}</ListGroup.Item>
+                        <ListGroup.Item action onClick={()=>moreInformation(movie.id)} variant="dark">{i} {movie.title}</ListGroup.Item>
                     </ListGroup>
                     :
                     <ListGroup>
-                        <ListGroup.Item action variant="light">{i} {movie.title}</ListGroup.Item>
+                        <ListGroup.Item action onClick={()=>moreInformation(movie.id)} variant="light">{i} {movie.title}</ListGroup.Item>
                     </ListGroup>
                 )
             })}
